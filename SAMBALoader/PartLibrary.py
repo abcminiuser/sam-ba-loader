4426c78168b1c1f7a9b211a4ab5dd129c622c43f
Dean Camera <dean@fourwalledcubicle.com>
2016-03-20 17:25:51 +1100

    More missing Python docstrings for the core module.

--- SAMBALoader/PartLibrary.py
+++ SAMBALoader/PartLibrary.py
@@ -14,11 +14,27 @@ import FlashControllers
 
 
 class PartLibrary(object):
+    """Part library class, which lists all supported devices and provides
+       methods to retrieve a given part by its chip identifiers, or by name.
+    """
+
     SUPPORTED_PARTS = [c() for c in Parts.SAMBAPart.__inheritors__]
 
 
     @staticmethod
     def get_chip_ids(samba):
+        """Reads out the chip identifiers from the attached device. Note that
+           each device usually implements only a single one of the chip
+           identifier modules, thus all but one value will essentially read as
+           garbage.
+
+           Args:
+               samba : Core `SAMBA` instance bound to the device.
+
+           Returns:
+               Dictionary of `{name, identifiers}` for each chip identifier,
+               which can then be used to match against a device.
+        """
         identifiers = dict()
 
         identifiers['CHIPID'] = ChipIdentifiers.CHIPID(base_address=0x400E0940)
@@ -32,9 +48,27 @@ class PartLibrary(object):
 
     @staticmethod
     def find_by_name(name):
+        """Retrieves a supported device by its name.
+
+           Args:
+              name : Name of the device to retrieve
+
+           Returns:
+              List of all parts whose name contains the search string.
+        """
         return [p for p in PartLibrary.SUPPORTED_PARTS if p.get_name() == name]
 
 
     @staticmethod
     def find_by_chip_ids(chip_ids):
+        """Retrieves a supported device by its chip identifiers.
+
+           Args:
+              chip_ids : Dictionary of `{name, identifiers}` for each chip
+                         identifier that should be matched against all supported
+                         devices.
+
+           Returns:
+              List of all parts which match against the chip identifiers.
+        """
         return [p for p in PartLibrary.SUPPORTED_PARTS for id_name, id_values in chip_ids.items() if p.identify(id_name, id_values)]
--- SAMBALoader/SAMBA.py
+++ SAMBALoader/SAMBA.py
@@ -13,6 +13,8 @@ import struct
 
 
 class SAMBACommands:
+    """Core SAM-BA bootloader commands."""
+
     SET_NORMAL_MODE = 'N'
     GO              = 'G'
     GET_VERSION     = 'V'
@@ -26,13 +28,33 @@ class SAMBACommands:
 
 
 class SAMBA(object):
+    """Main SAM-BA instance, used to issue commands to an attached device over
+       an established transport, and receive responses.
+    """
+
     def __init__(self, transport):
+        """Instantiates a SAMBA instance with a given transport, ready for use.
+
+           Args:
+               transport : Transport connected to a SAM-BA device.
+        """
+
         self.transport = transport
 
         self._execute(SAMBACommands.SET_NORMAL_MODE, read_length=2)
 
 
     def _to_32bit_hex(self, value):
+        """Internal helper function to convert a 32-bit value into a hex string,
+           if it is not already in a string representation.
+
+           Args:
+               value : A value to convert.
+
+           Returns:
+               Hex encoded string of the input value.
+        """
+
         if isinstance(value, str):
             return value
         else:
@@ -40,6 +62,19 @@ class SAMBA(object):
 
 
     def _execute(self, command, arguments=None, read_length=None):
+        """Executes a low level SAM-BA command, sending the command and
+           parameters to the device and reading back the response.
+
+           Args:
+               command     : `SAMBACommands` command to issue
+               arguments   : List of one or two arguments to send with the
+                             command, or `None` if no arguments should be sent.
+               read_length : Number of response bytes that are expected.
+
+           Returns:
+               Read response from the device after the command was issued.
+        """
+
         if arguments is None or len(arguments) is 0:
             arguments = ''
         elif len(arguments) is 1:
@@ -56,35 +91,94 @@ class SAMBA(object):
 
 
     def run_from_address(self, address):
+        """Starts execution in the attached device from the specified address.
+
+           Args:
+               address : Address in the attached device to run from
+        """
+
         self._execute(SAMBACommands.GO, arguments=[address], read_length=0)
 
 
     def get_version(self):
+        """Retrieves the SAM-BA version string from the attached device.
+
+           Returns:
+               Version string returned by the attached device.
+        """
+
         return self._execute(SAMBACommands.GET_VERSION).strip()
 
 
     def write_word(self, address, word):
+        """Writes a 32-bit word of data to the attached device.
+
+           Args:
+               address : Address to write the word at.
+               word    : 32-bit word of data to write
+        """
+
         self._execute(SAMBACommands.WRITE_WORD, arguments=[address, word], read_length=0)
 
 
     def read_word(self, address):
+        """Reads a 32-bit word of data from the attached device.
+
+           Args:
+               address : Address to read the word from.
+
+           Returns:
+               Word of data read from the attached device.
+        """
+
         word = self._execute(SAMBACommands.READ_WORD, arguments=[address], read_length=4)
         return struct.unpack("<I", word)[0]
 
 
     def write_half_word(self, address, half_word):
+        """Writes a 16-bit half-word of data to the attached device.
+
+           Args:
+               address   : Address to write the half-word at.
+               half_word : 16-bit half-word of data to write
+        """
         self._execute(SAMBACommands.WRITE_HALF_WORD, arguments=[address, half_word], read_length=0)
 
 
     def read_half_word(self, address):
+        """Reads a 16-bit half-word of data from the attached device.
+
+           Args:
+               address : Address to read the half-word from.
+
+           Returns:
+               Half-word of data read from the attached device.
+        """
+
         half_word = self._execute(SAMBACommands.READ_HALF_WORD, arguments=[address], read_length=2)
         return struct.unpack("<H", half_word)[0]
 
 
     def write_byte(self, address, byte):
+        """Writes an 8-bit byte of data to the attached device.
+
+           Args:
+               address : Address to write the byte at.
+               byte    : Byte of data to write
+        """
+
         self._execute(SAMBACommands.WRITE_BYTE, arguments=[address, byte], read_length=0)
 
 
     def read_byte(self, address):
+        """Reads a byte of data from the attached device.
+
+           Args:
+               address : Address to read the byte from.
+
+           Returns:
+               Byte of data read from the attached device.
+        """
+
         byte = self._execute(SAMBACommands.READ_BYTE, arguments=[address], read_length=1)
         return struct.unpack("<B", byte)[0]
